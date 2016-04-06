0dfc85feab4f20b96fa094c6b0f5309c6d3394c1
Andi Fajar <andifni@gmail.com>
2016-04-07 02:14:08 +0700

    Add android studio project to Git

---
+++ app/src/main/java/ppl/b08/warunglaundry/Home.java
@@ -0,0 +1,13 @@
+package ppl.b08.warunglaundry;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class Home extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
