022b907f4bbc17acda60fad046eacc8199ca461d
Andi Fajar <andifni@gmail.com>
2016-04-29 19:38:01 +0700

    add order sceleton to view

--- app/src/main/java/ppl/b08/warunglaundry/view/pengguna/OrderFragment.java
+++ app/src/main/java/ppl/b08/warunglaundry/view/pengguna/OrderFragment.java
@@ -1,11 +1,13 @@
 package ppl.b08.warunglaundry.view.pengguna;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import ppl.b08.warunglaundry.R;
 
@@ -24,7 +26,34 @@ public class OrderFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order, container, false);
+        View v = inflater.inflate(R.layout.fragment_order, container, false);
+        Button newOrderBtn = (Button) v.findViewById(R.id.new_order_btn);
+        Button historyOrderBtn = (Button) v.findViewById(R.id.history_order_btn);
+        Button currentOrderBtn = (Button) v.findViewById(R.id.current_order_btn);
+
+        newOrderBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), OrderNewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        historyOrderBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO
+            }
+        });
+
+        currentOrderBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO
+            }
+        });
+
+        return v;
     }
 
 }
