d995edb969878ae8f7db6bd6e3b69ccf7960c2f4
Andi Fajar <andifni@gmail.com>
2016-04-30 17:29:15 +0700

    GMaps for LProvider animate slowly

---
+++ app/src/main/java/ppl/b08/warunglaundry/view/pengguna/OrderNewProfileActivity.java
@@ -0,0 +1,15 @@
+package ppl.b08.warunglaundry.view.pengguna;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ppl.b08.warunglaundry.R;
+
+public class OrderNewProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_new_profile);
+    }
+}
