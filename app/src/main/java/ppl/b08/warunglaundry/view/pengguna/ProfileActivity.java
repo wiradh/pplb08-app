af1b20c2a1419c9c2acdc164fc3e2b8901457ac7
Bear <bps.bimo@gmail.com>
2016-05-26 05:49:37 +0700

    Buat update profile, sedikit pembenaran pada pesan login layout profile dan update profile

--- app/src/main/java/ppl/b08/warunglaundry/view/pengguna/ProfileActivity.java
+++ app/src/main/java/ppl/b08/warunglaundry/view/pengguna/ProfileActivity.java
@@ -2,10 +2,14 @@ package ppl.b08.warunglaundry.view.pengguna;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import ppl.b08.warunglaundry.R;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private EditText namaEdt;
+    private EditText noHPEdt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
