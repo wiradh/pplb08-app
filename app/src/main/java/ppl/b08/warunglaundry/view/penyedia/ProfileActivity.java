07d682ba25106abc86d3be6c004c5dd6cc55d2ff
Bear <bps.bimo@gmail.com>
2016-05-28 18:30:24 +0700

    Membuat agar data penyedia jasa dapat terlihat semua

--- app/src/main/java/ppl/b08/warunglaundry/view/penyedia/ProfileActivity.java
+++ app/src/main/java/ppl/b08/warunglaundry/view/penyedia/ProfileActivity.java
@@ -14,6 +14,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -27,13 +28,14 @@ import ppl.b08.warunglaundry.business.VolleySingleton;
 
 public class ProfileActivity extends AppCompatActivity {
     String id;
+    String nama;
     TextView namaEdt;
     TextView emailEdt;
     TextView noHPEdt;
-    TextView alamat1;
-    TextView harga1;
-    TextView rate1;
-    TextView jangkauan1;
+    TextView alamatEdt;
+    TextView hargaEdt;
+    TextView rateEdt;
+    TextView jangkauanEdt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,10 +44,10 @@ public class ProfileActivity extends AppCompatActivity {
         namaEdt = (TextView) findViewById(R.id.name1);
         emailEdt = (TextView) findViewById(R.id.email1);
         noHPEdt = (TextView) findViewById(R.id.telpon1);
-        alamat1 = (TextView) findViewById(R.id.alamat1);
-        harga1 = (TextView) findViewById(R.id.harga1);
-        rate1 = (TextView) findViewById(R.id.rate1);
-        jangkauan1 = (TextView) findViewById(R.id.jangkauan1);
+        alamatEdt = (TextView) findViewById(R.id.alamat1);
+        hargaEdt = (TextView) findViewById(R.id.harga1);
+        rateEdt = (TextView) findViewById(R.id.rate1);
+        jangkauanEdt = (TextView) findViewById(R.id.jangkauan1);
         Button updateBtn = (Button) findViewById(R.id.ubah_btn);
         Button kembali = (Button) findViewById(R.id.back_btn);
         getData();
@@ -77,7 +79,9 @@ public class ProfileActivity extends AppCompatActivity {
                 try {
                     JSONObject hasil = new JSONObject(response);
                     id = hasil.getString("id");
+                    nama = hasil.getString("username");
                     getDetails();
+                    getListLaundry();
                 } catch (JSONException e) {
                     Toast.makeText(ProfileActivity.this, "Kesalahan jaringan, silahkan coba lagi", Toast.LENGTH_SHORT).show();
                     e.printStackTrace();
@@ -104,10 +108,7 @@ public class ProfileActivity extends AppCompatActivity {
                     int status = hasil.getInt("status");
                     if (status == 1 ){
                         JSONObject user = hasil.getJSONObject("user");
-                        namaEdt.setText(user.getString("name"));
-                        noHPEdt.setText(user.getString("nomor_hp"));
                         emailEdt.setText(user.getString("email"));
-                        System.out.println(response);
                     }
                     else{
                         Toast.makeText(ProfileActivity.this, "Terjadi kesalahan, silahkan coba kembali", Toast.LENGTH_SHORT).show();
@@ -129,6 +130,51 @@ public class ProfileActivity extends AppCompatActivity {
                 HashMap<String, String> a = new HashMap<>();
                 a.put("token", PreferencesManager.getInstance(ProfileActivity.this).getToken());
                 a.put("id",id);
+                return a;
+            }
+        };
+        VolleySingleton.getInstance(this).addToRequestQueue(req);
+    }
+
+    public void getListLaundry() {
+
+        String url = C.HOME_URL+"/getLaundry";
+        StringRequest req = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject hasil = new JSONObject(response);
+
+                    JSONArray arr = hasil.getJSONArray("laundry");
+                    for (int i = 0; i < arr.length(); i++) {
+                        JSONObject laundry = (JSONObject) arr.get(i);
+                        if (nama.equals(laundry.getString("nama"))){
+                            namaEdt.setText(laundry.getString("nama"));
+                            hargaEdt.setText(laundry.getString("harga"));
+                            alamatEdt.setText(laundry.getString("alamat"));
+                            rateEdt.setText(laundry.getString("rate"));
+                            jangkauanEdt.setText(laundry.getString("jangkauan"));
+                            noHPEdt.setText(laundry.getString("telepon"));
+                            return;
+                        }
+                    }
+                } catch (JSONException e) {
+                    Toast.makeText(ProfileActivity.this, "Kesalahan jaringan, silahkan coba lagi", Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(ProfileActivity.this, "Kesalahan jaringan, silahkan coba lagi", Toast.LENGTH_SHORT).show();
+                error.printStackTrace();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String, String> a = new HashMap<>();
+                a.put("lattitude", "0");
+                a.put("longitude", "1");
                 return a;
             }
         };
